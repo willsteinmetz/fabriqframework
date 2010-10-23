054298e58bc83c6c57b6732ad3689d9c4ccfac7b
willsteinmetz <willsteinmetz@46d54a3c-7144-40a5-b641-3a42f7ee2fb6>
2010-10-23 18:45:07 +0000

    Updated license text to reduce file sizes
    git-svn-id: https://fabriq.svn.sourceforge.net/svnroot/fabriq/trunk@86 46d54a3c-7144-40a5-b641-3a42f7ee2fb6

--- app/controllers/errors.controller.php
+++ app/controllers/errors.controller.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * @file errors.controller.php
+ * The file helps manage errors
+ * @author Will Steinmetz
+ * 
+ * Copyright (c)2010, Ralivue.com
+ * Licensed under the BSD license.
+ * http://fabriqframework.com/license
+ */
 class errors_controller extends Controller {
 	function fourohfour() {
 		
