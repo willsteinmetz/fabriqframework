e5ad75a96947ee9e85b499dab08ee4cc7cbb74b5
willsteinmetz <willsteinmetz@46d54a3c-7144-40a5-b641-3a42f7ee2fb6>
2010-11-27 04:28:11 +0000

    Updated core JavaScript file
    git-svn-id: https://fabriq.svn.sourceforge.net/svnroot/fabriq/branches/fabriq-1.3@110 46d54a3c-7144-40a5-b641-3a42f7ee2fb6

--- core/fabriq.js
+++ core/fabriq.js
@@ -6,59 +6,62 @@
  * Licensed under the BSD license.
  * http://fabriqframework.com/license
  */
-var Fabriq;
-if (!Fabriq) Fabriq = {'settings': {}, 'custom': {}};
 
-/**
- * Returns the base path for the app
- */
-Fabriq.base_path = function() {
-	return this.settings.basePath;
-}
-
-/**
- * Returns whether or not clean URLs are used
- */
-Fabriq.clean_urls = function() {
-	return this.settings.cleanURLs;
-}
-
-/**
- * Returns the string for an ajax path
- */
-Fabriq.ajax_path = function() {
-	var path = '';
-	if (!this.clean_urls()) {
-		path += 'index.php?q=';
-	} else {
-		path += this.base_path();
-	}
-	args = arguments;
-	path += args[0];
-	if (args.length > 1) {
-		path += '/' + args[1]
-	}
+Fabriq = {
+	settings: {},
+	custom: {},
 	
-	return path;
-}
-
-/**
- * Returns the string for a path
- */
-Fabriq.build_path = function() {
-	var path = '';
-	if (!this.clean_urls()) {
-		path += 'index.php?q=';
-	} else {
-		path += this.base_path();
-	}
-	args = arguments;
-	path += args[0];
-	if (args.length > 1) {
-		for (var i = 1; i < args.length; i++) {
-			path += '/' + args[i];
+	/**
+	 * Returns the base path for the app
+	 */
+	base_path: function() {
+		return this.settings.basePath;
+	},
+	
+	/**
+	 * Returns whether or not clean URLs are used
+	 */
+	clean_urls: function() {
+		return this.settings.cleanURLs;
+	},
+	
+	/**
+	 * Returns the string for an ajax path
+	 */
+	ajax_path: function() {
+		var path = '';
+		if (!this.clean_urls()) {
+			path += 'index.php?q=';
+		} else {
+			path += this.base_path();
 		}
-	}
+		args = arguments;
+		path += args[0];
+		if (args.length > 1) {
+			path += '/' + args[1]
+		}
+		
+		return path;
+	},
 	
-	return path;
+	/**
+	 * Returns the string for a path
+	 */
+	build_path: function() {
+		var path = '';
+		if (!this.clean_urls()) {
+			path += 'index.php?q=';
+		} else {
+			path += this.base_path();
+		}
+		args = arguments;
+		path += args[0];
+		if (args.length > 1) {
+			for (var i = 1; i < args.length; i++) {
+				path += '/' + args[i];
+			}
+		}
+		
+		return path;
+	}
 }
