4880d7e6a75b16cf9a58b782b6225d305f4c1a35
Will Steinmetz <willsteinmetz@me.com>
2014-12-07 16:12:38 -0500

    Transformed Application controller to be the base controller class

--- app/controllers/application.controller.php
+++ app/controllers/application.controller.php
@@ -2,9 +2,28 @@
 /**
  * @file Controller file containing application-wide processing
  * functions
- * 
+ *
  * Copyright (c)2011, Ralivue.com
  * Licensed under the BSD license.
  * http://fabriqframework.com/license
  */
 
+namespace Fabriq\App\Controllers {
+  class Application {
+    /**
+     * Default actions to perform before an action is called
+     * Always call the parent when overridden
+     */
+    public function before() {
+
+    }
+
+    /**
+     * Default actions to perform after an action is called
+     * Always call the parent when overridden
+     */
+    public function after() {
+
+    }
+  }
+}
